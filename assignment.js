4f4b264b98014837376007cff24a0e5f2c65d6fc
Sonal Tiwary <136879126+sonaltiwary7@users.noreply.github.com>
2023-09-28 19:31:36 +0530

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,50 @@
+// comments can make code readable
+// Welcome to 30DaysOfJavaScript
+/* comments can make code readable,
+easy to reuse 
+and informative */
+
+// question 4
+let name = "Sonal";
+let bool = true;
+let rollNo = null;
+let dog;
+
+// question 5
+let name = "Sonal";
+let bool = true;
+let rollNo = null;
+let dog;
+console.log(typeof name);
+console.log(typeof isActive);
+console.log(typeof rollNo);
+console.log(typeof dog);
+
+// question 6
+let Talkno. ;
+let firstname ;
+let vehicle ;
+let folder ;
+
+// question 7
+let talkno. = 25;
+let firstname = "Sonal";
+let vehicle = "GT";
+let folder = "here";
+
+// question 8
+let firstName = "Sonal";
+let lastName = "tiwary";
+let maritalStatus = "Married";
+let country = "United Kingdom";
+let age = 30;
+
+// question 9
+let firstName = "Sonal", lastName = "tiwary", maritalStatus = "Married", country = "United Kingdom", age = 30;
+
+// 10
+let myAge = 21;
+let yourAge = 20;
+
+console.log("I am " + myAge +"years old");
+console.log("You are" + yourAge +"years old");
